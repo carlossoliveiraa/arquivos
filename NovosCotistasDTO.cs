37ac2389ce93474bd964280f213941b849bd9fac
Carlos A Oliveira <carlossoliveiraa@hotmail.com>
2021-09-14 13:48:45 -0300

    asd

---
+++ NovosCotistasDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using FileHelpers;
+
+namespace ArquivosPosicionados
+{
+
+    [FixedLengthRecord(FixedMode.ExactLength)]
+    public class NovosCotistasDTO
+    {
+        [FieldFixedLength(5)]
+        //[FieldAlign(AlignMode.Right)]
+        public string agencia;
+
+        [FieldFixedLength(13)]
+        public string conta;
+
+        [FieldFixedLength(10)]
+        [FieldConverter(ConverterKind.Date, "ddMMyyyy")] 
+        public DateTime dataInclusao;
+
+        [FieldFixedLength(1)]
+        public string tipo;
+
+        [FieldFixedLength(7)]
+        public string idUsuario;
+
+        [FieldFixedLength(14)]
+        public string cnpj;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using FileHelpers;
+using System;
+using System.Collections.Generic;
+
+namespace ArquivosPosicionados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           List<NovosCotistasDTO> lista = new List<NovosCotistasDTO>
+           {
+               new NovosCotistasDTO
+               {
+                   agencia = "0168",
+                   conta = "504122",
+                   dataInclusao = DateTime.Now,                  
+                   tipo = "5",
+                   idUsuario = "1",
+                   cnpj = "23.160.569/0001-10",
+               }
+               
+           };
+
+            EscrevaArquivo(@"C:\Projetos\Publicacao\teste20210909.txt", lista);
+
+        }
+
+        public static void EscrevaArquivo(string nomeDoArquivo, List<NovosCotistasDTO> registros)
+        {
+            FileHelperEngine engine = new FileHelperEngine(typeof(NovosCotistasDTO));
+
+            engine.WriteFile(nomeDoArquivo, registros);
+        }
+    }
+}
